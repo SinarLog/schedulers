f1e6c74834293867b96d572e465cb3439391b9d2
Rifky Manuel Satyana <rifkymanuelsatyana@gmail.com>
2023-07-31 23:12:25 +0700

    fix: changes health check timer

--- jobs/check.go
+++ jobs/check.go
@@ -13,17 +13,19 @@ func HealthCheck(scheduler *gocron.Scheduler, db *gorm.DB, redis *redis.Client) 
 		// Check db conn
 		if err := db.WithContext(job.Context()).Raw("SELECT 1=1 AS t").Error; err != nil {
 			log.Printf("db conn problem for HealthCheck: %s\n", err)
+			return
 		}
 
 		// Check redis conn
 		if err := redis.Ping(job.Context()).Err(); err != nil {
 			log.Printf("redis conn problem for HealthCheck: %s\n", err)
+			return
 		}
 
 		log.Printf("health check completed")
 	}
 
-	_, err := scheduler.Every(1).Minute().Tag("healthCheck").DoWithJobDetails(task)
+	_, err := scheduler.Every(30).Minute().Tag("healthCheck").DoWithJobDetails(task)
 
 	return err
 }
